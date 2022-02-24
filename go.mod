969a1bda47512773cf31b745b714fe7855d0cfc1
João Cerqueira <joao@cerqueira.io>
2022-02-24 15:38:21 +0000

    feat: init commit

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/crqra/conventional-commits-action
+
+go 1.17
+
+require github.com/crqra/go-action v0.0.0-20220223095229-fb2cc88de0f0
+
+require (
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/google/go-github/v42 v42.0.0 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
+	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"regexp"
+
+	"github.com/crqra/go-action/pkg/action"
+)
+
+var patterns = []*regexp.Regexp{
+	regexp.MustCompile("(?im)^breaking change:.*"),
+	regexp.MustCompile(`(?i)^(\w+)(\(.*\))?!:.*`),
+	regexp.MustCompile(`(?i)^feat(\(.*\))?:.*`),
+	regexp.MustCompile(`(?i)^fix(\(.*\))?:.*`),
+	regexp.MustCompile(`(?i)^docs(\(.*\))?:.*`),
+	regexp.MustCompile(`(?i)^ci(\(.*\))?:.*`),
+	regexp.MustCompile(`(?i)^build(\(.*\))?:.*`),
+}
+
+type PullRequestEvent struct {
+	PullRequest struct {
+		Title string `json:"title"`
+	} `json:"pull_request"`
+}
+
+func (evt PullRequestEvent) String() string {
+	return evt.PullRequest.Title
+}
+
+type PushEvent struct {
+	Commits []struct {
+		ID      string `json:"id"`
+		Message string `json:"message"`
+	} `json:"commits"`
+}
+
+type ConventionalCommitsAction struct{}
+
+func (a *ConventionalCommitsAction) Run() error {
+	switch action.Context.EventName {
+	case "pull_request":
+		var evt PullRequestEvent
+
+		if err := parseEvent(&evt); err != nil {
+			return err
+		}
+
+		return validatePullRequest(evt)
+
+	case "push":
+		var evt PushEvent
+
+		if err := parseEvent(&evt); err != nil {
+			return err
+		}
+
+		return validatePush(evt)
+	}
+
+	return nil
+}
+
+func main() {
+	if err := action.Execute(&ConventionalCommitsAction{}); err != nil {
+		action.SetFailed(err, map[string]string{})
+	}
+}
+
+func validatePullRequest(evt PullRequestEvent) error {
+	valid := false
+
+	for _, p := range patterns {
+		valid = p.MatchString(evt.PullRequest.Title)
+	}
+
+	if !valid {
+		return fmt.Errorf("Pull Request title is not a valid Conventional Commit")
+	}
+
+	return nil
+}
+
+func validatePush(evt PushEvent) error {
+	for _, c := range evt.Commits {
+		valid := false
+
+		for _, p := range patterns {
+			valid = p.MatchString(c.Message)
+		}
+
+		if !valid {
+			return fmt.Errorf("Commit %s is not a valid Conventional Commit", c.ID)
+		}
+	}
+
+	return nil
+}
+
+func parseEvent(evt interface{}) error {
+	data, err := os.ReadFile(action.Context.EventPath)
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal(data, evt)
+}
